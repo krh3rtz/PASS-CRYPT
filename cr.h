dd4ab4208bba7c12ebce25f9bf4d1904f4a7f4a3
krhertz <krhertz@users.noreply.github.com>
2017-02-07 22:48:51 -0600

    cr.h

--- cr.h
+++ cr.h
@@ -1,7 +1,7 @@
 /*
  cr.h: This file contains all the functions used in the crypter.
  
- * Author Krhertz 2017
+ * Author Krhertz 2016
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
