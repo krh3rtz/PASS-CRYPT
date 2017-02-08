4130a561506098da9726d5c65f8707e2df6e36c7
krhertz <krhertz@users.noreply.github.com>
2017-02-07 22:48:31 -0600

    PASS-CRYPT.c

--- PASS-CRYPT.c
+++ PASS-CRYPT.c
@@ -1,7 +1,7 @@
 /*
   PASS-CRYPT
 
- * Copyright (C) 2016 Krhertz
+ * Author Krhertz 2016
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
